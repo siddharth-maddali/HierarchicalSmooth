4f6698ee19c558f22953a9f7ecd4222010d9f056
Siddharth  Maddali <siddharth.mv@gmail.com>
2016-11-10 01:31:13 -0500

    Changelog for branch cpp-module: 1) Completed function HierarchicalSmooth.GraphLaplacian to mimic matlab version 2) Started function HierarchicalSmooth.Smooth, using Python's None object to    deal with variable number of input arguments.

--- Src/Python/HierarchicalSmooth.pyx
+++ Src/Python/HierarchicalSmooth.pyx
@@ -2,8 +2,6 @@ import numpy as np
 from scipy.sparse import diags
 import Base as base
 
-import Base as base
-
 def FastChainLinkSort( FBIn ):
     FBOut = FBIn
     for i in range( 1, FBOut.shape[0] ):
@@ -20,13 +18,23 @@ def Laplacian2D( N ):
     return M
 
 def GraphLaplacian( tri ):
-   nUniq = np.unique( tri )
-   nIdx = range( nUniq.size )
-   nSubTri = np.array( base.ismember( tri, nUniq )[1] ).reshape( -1, tri.shape[1] )
-   MLap = diags( [ np.zeros( nUniq.size ) ], [ 0 ] )
-   return MLap, nUniq
+    nUniq = np.unique( tri )
+    nIdx = range( nUniq.size )
+    nSubTri = np.array( base.ismember( tri, nUniq )[1] ).reshape( -1, tri.shape[1] )
+    MLap = diags( [ np.zeros( nUniq.size ) ], [ 0 ] ).tolil()    # linked list sparse matrix
+    for i in range( nSubTri.shape[0] ):
+        for j in range( 3 ):
+            l = ( j + 3 ) % 3
+            m = ( j + 4 ) % 3 
+            n = ( j + 5 ) % 3
+            MLap[ nSubTri[i,l], nSubTri[i,m] ] = -1
+            MLap[ nSubTri[i,l], nSubTri[i,n] ] = -1
+    for i in range( MLap.shape[0] ):
+        MLap[ i, i ] = -MLap[i,:].sum()
+    return MLap, nUniq
     
-
+    def Smooth( yIn, fThreshold, nMaxIterations, L=None, nFixed=None ):
+        
 
 
 
