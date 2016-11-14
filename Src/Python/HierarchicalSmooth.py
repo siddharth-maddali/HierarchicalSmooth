4e297d6e915d42d831dc026884a8b212c2a00c65
Siddharth  Maddali <siddharth.mv@gmail.com>
2016-11-14 00:43:28 -0500

    Changelog for branch cpp-module: 1) .gitignore now rejects all compiled files and shared/static libraries. 2) Completed HierarchicalSmooth.Smooth 3) New module HierarchicalSmooth_PRIVATE contains aux functions used in HiererchicalSmooth

--- Src/Python/HierarchicalSmooth.py
+++ Src/Python/HierarchicalSmooth.py
@@ -1,6 +1,12 @@
 import numpy as np
-from scipy.sparse import diags
-import Base as base
+from scipy.sparse import diags, csc_matrix  # to create sparse diagonal matrices
+from scipy.sparse.linalg import spsolve     # to solve sparse systems of equations
+
+#import Base as base                         # custom module containing ismember, etc.
+import HierarchicalSmooth_PRIVATE as hspv   # private functions to make this code mode readable
+
+import copy                                 # to use deepcopy
+import sys                                  # to write to output streams
 
 def FastChainLinkSort( FBIn ):
     FBOut = FBIn
@@ -15,7 +21,7 @@ def Laplacian2D( N ):
     M = diags( [ np.ones( N-1 ) ], [ 1 ] ) - diags( [ np.ones( N ) ], [ 0 ] )
     M = M.T * M
     M[ -1, -1 ] = 1.
-    return M
+    return M.tocsc()
 
 def GraphLaplacian( tri ):
     nUniq = np.unique( tri )
@@ -31,9 +37,62 @@ def GraphLaplacian( tri ):
             MLap[ nSubTri[i,l], nSubTri[i,n] ] = -1
     for i in range( MLap.shape[0] ):
         MLap[ i, i ] = -MLap[i,:].sum()
-    return MLap, nUniq
+    return MLap.tocsc(), nUniq
     
-#    def Smooth( yIn, fThreshold, nMaxIterations, L=None, nFixed=None ):
+def Smooth( yInArray, fThreshold, nMaxIterations, L=None, nFixed=None ):
+
+    if L==None and nFixed==None:
+        N = yInArray.shape[0]
+        L = Laplacian2D( N )
+        nFixed = [ 0, N-1 ]
+
+    nMobile = [ i for i in range( N ) if i not in nFixed ]
+    yIn = csc_matrix( yInArray )
+
+    if len( nMobile ) < 1:
+        return yIn, []
+
+    IterData = []
+    LRed = L[ :, nMobile ][ nMobile, : ]
+    fConst = copy.deepcopy( L )
+    fConst[ :, nMobile ] = 0
+    fConst = ( fConst * yIn )[ nMobile, : ]
+    D = L.multiply( L > 0. ) # diagonal matrix
+    A = L.multiply( L < 0. ) # adjacency matrix
+    AyIn = A * yIn
+    fSmallEye = diags( [ np.ones( len( nMobile ) ) ], [ 0 ] )
+    yMobile = yIn[ nMobile, : ]
+
+    fEps = 0.5
+    fStep = fEps/2.
+    nIterations = 1
+    LRedTLRed = LRed.T * LRed
+    LRedConst = LRed.T * fConst
+
+    fObj1 = hspv._GetObjFn( fEps, fSmallEye, LRedTLRed, yIn, nMobile, LRedConst, D, AyIn )[0]
+    fObj2 = hspv._GetObjFn( fEps+fThreshold, fSmallEye, LRedTLRed, yIn, nMobile, LRedConst, D, AyIn )[0]
+
+    fSlope = ( fObj2 - fObj1 ) / fThreshold
+    while abs( fSlope ) > fThreshold and nIterations < nMaxIterations:
+        if fSlope > 0.:
+            fEps -= fStep
+        else:
+            fEps += fStep
+
+        fStep /= 2.
+        fObj1, yOut = hspv._GetObjFn( fEps, fSmallEye, LRedTLRed, yIn, nMobile, LRedConst, D, AyIn )
+        fObj2 = hspv._GetObjFn( fEps+fThreshold, fSmallEye, LRedTLRed, yIn, nMobile, LRedConst, D, AyIn )[0]
+
+        fSlope = ( fObj2 - fObj1 ) / fThreshold
+        IterData.append( [ fEps, fObj1 ] )
+        nIterations += 1
+
+    if nIterations == nMaxIterations:
+        sys.stderr.write( 'Smooth warning: Max. number of iterations reached.\n' )
+    return yOut, IterData, nIterations
+
+
+
         
 
 
