3ae5ecf6417445c38b69f25daa4547a2dced7ce3
Siddharth Maddali <siddharth.mv@gmail.com>
2016-11-17 17:07:20 -0500

    Changelog for branch cpp-module: 1) HierarchicalSmooth.FastChainLinkSort: Modified how FBOut rows are swapped.

--- Src/Python/HierarchicalSmooth.py
+++ Src/Python/HierarchicalSmooth.py
@@ -14,7 +14,7 @@ def FastChainLinkSort( FBIn ):
     FBOut = FBIn
     for i in range( 1, FBOut.shape[0] ):
         f = np.where( np.any( FBOut[i:,:]==FBOut[i-1,1], axis=1 ) )[0]
-        FBOut[ [ i, f+i ], : ] = FBOut[ [ f+i, i ], : ]
+        FBOut[ [i] + [ i+j for j in f ] ] = FBOut[ [ i+j for j in f ] + [i] ] 
         if FBOut[ i,1 ] == FBOut[ i-1,1 ]:
             FBOut[ i, [ 0, 1 ] ] = FBOut[ i, [ 1, 0 ] ]
     return FBOut
