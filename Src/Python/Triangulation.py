f5e4af8a3caa0b3937df8645d8685e6f35748d62
Siddharth Maddali <siddharth.mv@gmail.com>
2016-12-01 17:09:19 -0500

    Changelog for branch cpp-module: Modified Triangulation._FastChainLinkSort to smoothly accommodate right-handed indexing of mesh elements nodes. As a result, the code is much more compact and intuitive, but as a side effect the routine can return point-connected mesh elements along a surface perimeter as separate surfaces. This is written off conceptually as the inability of the mesh to resolve the surface further.

--- Src/Python/Triangulation.py
+++ Src/Python/Triangulation.py
@@ -15,52 +15,27 @@ def __SortMinMax( arr ):
         axis=1 
     )
 
-def _FastChainLinkSort( fe, tri ):
-    MyDict = {}
-    for Edge in fe:
-        EdgeS = str( Edge[0] )
-        while MyDict.has_key( EdgeS ):
-            EdgeS += '.'
-        MyDict[ EdgeS ] = Edge[1]
-    MyList = []
-    Key = MyDict.keys()[0]
-    MyList.append( [ int( Key.strip( '.' ) ), MyDict[ Key ] ] )
-    OldKey = Key
-    Key = str( MyDict[ Key ] )
-    del MyDict[ OldKey ]
-    while len( MyDict ) > 0:
-        Duplicates = []
-        TempKey = Key.strip( '.' )
-        while MyDict.has_key( TempKey ):
-            Duplicates.append( TempKey )
-            TempKey += '.'
-
-        n = 0 
-        if len( Duplicates ) > 1:
-            TriangleWithThisEdge = set( [ list( i ) for i in tri if 
-                int( OldKey.strip('.') ) in i and 
-                int( Key ) in i 
-            ][0] )
-            while n < len( Duplicates ):
-                TriangleWithNextEdge = set( [ list( i ) for i in tri if 
-                    int( Duplicates[n].strip('.') ) in i and
-                    MyDict[ Duplicates[n] ] in i
-                ][0] )
-                if len( TriangleWithThisEdge.intersection( TriangleWithNextEdge ) )==1:
-                    break
-                n += 1
+def _FastChainLinkSort( fe ):
+    TrgDict = {}
+    for Edge in fe: # hashing edges...
         try:
-            Key = Duplicates[n]
-        except IndexError:
-            Key = MyDict.keys()[0]
-        MyList.append( [ int( Key.strip( '.' ) ), MyDict[ Key ] ] )
-        OldKey = Key
-        try:
-            Key = str( MyDict[ Key ] )
+            TrgDict[ Edge[0] ].append( Edge[1] )
         except KeyError:
-            Key = MyDict.keys()[0]
-        del MyDict[ OldKey ]
-    return np.array( MyList )
+            TrgDict[ Edge[0] ] = [ Edge[1] ] # create new list
+    List = np.zeros( ( len( fe ), len( fe[0] ) ), dtype=int )
+    Key = TrgDict.keys()[0]
+    for i in range( List.shape[0] ):
+        List[i,0], List[i,1] = Key, TrgDict[ Key ][-1]
+        NewKey = TrgDict[ Key ][-1]
+        TrgDict[ Key ].pop()
+        if len( TrgDict[ Key ] ) == 0:
+            del TrgDict[ Key ]
+        if TrgDict.has_key( NewKey ):
+            Key = NewKey
+        elif len( TrgDict ) > 0:
+            Key = TrgDict.keys()[0]
+
+    return List
 
 def _GetEdges( tri ):
     MyEdges = np.concatenate( ( tri[:,[ 0, 1 ] ], tri[:,[ 1, 2 ] ], tri[:,[ 2, 0 ] ] ), axis=0 )
@@ -78,7 +53,7 @@ def _GetEdges( tri ):
         AllEdges.append( MyDict[i].orig )
         if MyDict[i].count == 1:
             FreeEdges.append( MyDict[i].orig )
-    return np.array( AllEdges ), _FastChainLinkSort( FreeEdges, tri )
+    return np.array( AllEdges ), _FastChainLinkSort( FreeEdges )
 
 class Triangulation:
     def __init__( self, MyTri, X ):
@@ -91,7 +66,6 @@ class Triangulation:
 
     def freeBoundary( self ):
         return self.freeboundary
-
 
     def edges( self ):
         return self.edges
