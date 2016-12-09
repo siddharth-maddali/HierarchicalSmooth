701839bffa9a2009a75eef603c93e1851513dcc7
Siddharth Maddali <siddharth.mv@gmail.com>
2016-12-08 19:01:24 -0500

    cpp-module: Corrected very silly mistake in HSmoothMain::Laplacian2D

--- Src/Cpp/HierarchicalSmooth.cpp
+++ Src/Cpp/HierarchicalSmooth.cpp
@@ -11,7 +11,6 @@
 
 SpMat HSmoothMain::Laplacian2D( size_t N, std::string type ) {
 
-//	std::cout << "Hello world from HSmoothMain::Laplacian2D. " << '\n';
 	std::vector< T > tripletList;
 	tripletList.reserve( 3*N ); // approx. number of nonzero elements
 	for( size_t i = 0; i < N; i++ ) {
@@ -22,14 +21,17 @@ SpMat HSmoothMain::Laplacian2D( size_t N, std::string type ) {
 	SpMat temp( N, N );
 	temp.setFromTriplets( tripletList.begin(), tripletList.end() );
 	SpMat L = SpMat( temp.transpose() ) * temp;
-	if( type=="serial" )
-		L.insert( N-1, N-1 ) = 1.0;
+	if( type=="serial" ){ 
+		L.coeffRef( N-1, N-1 ) = 1.0;
+	}
 	else if( type=="cyclic" ) { 
-		L.insert( 0, 0 ) = 2.0;
-		L.insert( 0, N-1 ) = L.insert( N-1, 0 ) = -1.0;
+		L.coeffRef( 0, 0 ) = 2.0;
+		L.coeffRef( 0, N-1 ) = L.coeffRef( N-1, 0 ) = -1.0;
 	}
 	else
-		std::cerr << "HSmoothMain::Laplacian2D: Unrecognized type. " << '\n';
+		std::cerr << "HSmoothMain::Laplacian2D: Unrecognized type. " << std::endl;
+
+	return L;
 }
 
 //============================================================================================
